94cc8a5485cead8535c0552f881c587c1470bc40
mitchgm <mitchgm@bgsu.edu>
2022-10-06 01:56:17 -0700

    bootstrap intro

--- new/src/App.js
+++ new/src/App.js
@@ -4,13 +4,31 @@ class NavBar extends Component {
 
     render() {
         return (
-        <div>
-        <h1>Hello WOrld</h1>
-        <p>p</p>
-        </div>
+        <React.Fragment>
+
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">hook-em-up</a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+
+
+    </ul>
+
+  </div>
+</nav>
+        </React.Fragment>
+  
         );
-    
+        }
     }
-}
 
 export default NavBar;
