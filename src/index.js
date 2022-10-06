5b2eceb2db31623caf618839bfb91e42d85d0a86
mitchgm <mitchgm@bgsu.edu>
2022-10-06 00:27:43 -0700

    react start

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "jquery";
 import "popper.js/dist/umd/popper";
-
 import "bootstrap/dist/js/bootstrap";
-
 import "bootstrap/dist/css/bootstrap.css";
+import {NavBar} from "./App";
 import App from "./App";
 import "./index.css";
 import "font-awesome/css/font-awesome.css";
