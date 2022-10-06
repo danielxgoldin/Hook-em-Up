b9ba54df424bc60dec942ab403b45a3366c3603d
mitchgm <mitchgm@bgsu.edu>
2022-10-05 23:32:48 -0700

    sample html quickset

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "jquery";
 import "popper.js/dist/umd/popper";
+
 import "bootstrap/dist/js/bootstrap";
+
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import "./index.css";
