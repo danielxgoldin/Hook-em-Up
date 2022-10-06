c5f9d192e188a723485d31e2bd78310b189cd123
mitchgm <mitchgm@bgsu.edu>
2022-10-06 06:22:26 -0700

    additional js files added

--- new/src/App.js
+++ new/src/App.js
@@ -1,34 +1,15 @@
 import React, {Component} from "react";
-
-class NavBar extends Component {
-
-    render() {
-        return (
-        <React.Fragment>
-
-       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">hook-em-up</a>
-
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-
-      <li class="nav-item">
-        <a class="nav-link" href="#">Home</a>
-      </li>
+import NavBar from "./NavBar";
+import MainContent from "./MainContent";
 
 
-    </ul>
-
-  </div>
-</nav>
-        </React.Fragment>
-  
-        );
-        }
+export default class App extends Component 
+{
+    render()
+    {
+        return <React.Fragment>
+            <NavBar></NavBar>
+            <MainContent></MainContent>
+            </React.Fragment>
     }
-
-export default NavBar;
+}
--- new/src/index.js
+++ new/src/index.js
@@ -5,8 +5,8 @@ import "jquery";
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-import NavBar from "./App.js";
+import App from "./App.js";
 import "./index.css";
 
 
-ReactDOM.render(<NavBar />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
