432c371209c7107146d3593d9c7be0c5e743eb3a
Zaidi <zaidiahsan.r@gmail.com>
2022-10-06 16:44:32 -0500

    styling

--- hook-em-up/src/components/Navbar.js
+++ hook-em-up/src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../styles/navbar.css';
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingBag } from '@fortawesome/free-solid-svg-icons';
 //import { faCoffee } from '@fortawesome/free-solid-svg-icons'   
 
 const Navbar = () => {
@@ -20,12 +23,24 @@ const Navbar = () => {
                 </div>
             </div>
             <div className='n-row'>
+                <div className='n-col'>
+                    <a href='/'><img src='/images/logo/logo.png' className='logo' alt='' /></a>
+                </div>
+                <div className='n-col'>
+                    <div className='icons'>
+                        <a href='/'><span><FontAwesomeIcon icon={faArrowRightToBracket} />Login</span></a>
+                        <a href='/'><span><FontAwesomeIcon icon={faHeart} /><span className='totalItem'>0</span></span></a>
+                        <a href='/'><span><FontAwesomeIcon icon={faShoppingBag} /><span className='totalItem'>0</span></span></a>
+                    </div>
+                </div>
+            </div>
+            <div className='n-row'>
                 <nav className='nav'>
                     <ul className='items'>
-                        <li className='list'></li>
-                        <li className='list'></li>
-                        <li className='list'></li>
-                        <li className='list'></li>
+                        <li className='list'>Home</li>
+                        <li className='list'>Shop</li>
+                        <li className='list'>About</li>
+                        <li className='list'>Contact</li>
                     </ul>
                 </nav>
             </div>
