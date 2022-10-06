f908ce9f138387253c1b7a6c9a0dea2a8208bd37
mitchgm <mitchgm@bgsu.edu>
2022-10-06 07:36:21 -0700

    more resetup

--- hook-em-up/src/index.js
+++ hook-em-up/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import ReactDOM from "react-dom";
 import "jquery";
-// import "popper.js/dist/umd/popper";
+// import "popper/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App.js";
