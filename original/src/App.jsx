ab5ff616e580b02a187eea7daf79b22693d8f46b
mitchgm <mitchgm@bgsu.edu>
2022-10-06 01:12:47 -0700

    restarting react creating and triuble shooting

---
+++ original/src/App.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class NavBar extends Component {
+
+    render() {
+        return (
+        <div>
+        <h1>Hello WOrld</h1>
+        <p>p</p>
+        </div>
+        );
+    
+    }
+}
+
+export default NavBar;
---
+++ original/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+// import "jquery";
+// import "popper.js/dist/umd/popper";
+// import "bootstrap/dist/js/bootstrap";
+// import "bootstrap/dist/css/bootstrap.css";
+import NavBar from "./App.jsx";
+
+
+ReactDOM.render(<NavBar />, document.getElementById("root"));
+
+
+
+
+
