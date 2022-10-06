b051df4cff41ec080895ed292d44d127f05d2484
Zaidi <zaidiahsan.r@gmail.com>
2022-10-06 17:03:37 -0500

    Added components

---
+++ hook-em-up/src/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+        <Navbar></Navbar>
+
+        <blockquote className="blockquote ">
+        <p style={{ textAlign: "justify" }}>
+            Hello Everyone, My name is <strong >Ahsan Zaidi. </strong> 
+            The Full-stack web developer with a background in computer science and a passion for coding.
+            I’ve always sought out opportunities and challenges that are meaningful to me. 
+            Skilled in designing, developing and implementing applications and solutions using a range of technologies and programming languages. 
+            Aspiring to combine broad background with strong technical skills to excel as a Full Stack Developer.
+            <br />
+        
+        </p>
+                
+
+            </blockquote>       
+        </div>
+    )
+}
+
+export default Home;
