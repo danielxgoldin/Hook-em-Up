be6959897cbcc39df2791a44553a5dd97863a9c8
mitchgm <mitchgm@bgsu.edu>
2022-10-06 17:54:38 -0700

    updating example codes

--- hook-em-up/src/index.js
+++ hook-em-up/src/index.js
@@ -5,7 +5,7 @@ import "jquery";
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-import App from "./App.js";
+import App from "./App.jsx";
 import "./index.css";
 
 
