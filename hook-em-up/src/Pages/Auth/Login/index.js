8014dcf440e3a79789f3e7a5594c90a1037a8fde
Zaidi <zaidiahsan.r@gmail.com>
2022-10-08 17:39:05 -0500

    login form

--- hook-em-up/src/Pages/Auth/Login/index.js
+++ hook-em-up/src/Pages/Auth/Login/index.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
+    const { user, login, setUser, setIsSubmitting, loggedIn } = useAuth()
 
     const [ email, setEmail ] = useState("")
     const [ password, setPassword ] = useState("")
@@ -26,4 +27,65 @@ const Login = () => {
     useEffect(() => {
         loggedIn && navigate('/')
     }, [loggedIn])
+
+    return (
+        <div className='Container-group'>
+            <div className='Form-group'>
+                <div>
+                    <h2 className='title'>Login</h2>
+                </div>
+                <form
+                autoComplete='off'
+                onSubmit={handleLogin}
+                className="Login-form"
+                >
+                    <div className='input-group'>
+                        <div>
+                            <label className='in-put'>Email</label>
+                            <input
+                                type="Email"
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                                ref={emailRef}
+                                className="input"
+                                placeholder='Email Address'
+                                required   
+                            />
+                        </div>
+                        <div>
+                            <label className='in-put'>Password</label>
+                            <input
+                                type="Password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                                ref={passwordRef}
+                                className='input'
+                                placeholder='Password'
+                                required
+                            />
+                        </div>
+                        <div className='links'>
+                            <div className='links-div'>
+                                <span>
+                                    No Account? Sign Up{" "}
+                                    <Link to="/signup" className="text-black-600 hover:underline">
+                                        {" "}
+                                        here!
+                                    </Link>
+                                </span>
+                            </div>
+                        </div>
+                        <div className='text-center'>
+                            <button type='submit' className='button'>
+                                <LoginIcon className="my-auto h-5 w-6" aria1-hidden="true" />
+                                Login
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
+
+export default Login
