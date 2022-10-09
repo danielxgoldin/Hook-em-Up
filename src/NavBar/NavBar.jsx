882abb120399496777c533c76032cf4bc2c6dc09
mitchgm <mitchgm@bgsu.edu>
2022-10-09 12:13:46 -0700

    updating navbar titles

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -10,7 +10,41 @@ import { Link } from "react-router-dom";
 var navBar = ({}) => {
 
     return (
-        
+        <nav className="navbar navbar-dark">
+            {/* navbar title  */}
+
+            <a href="/#" className="navbar-title">Hook Em Up</a>
+            {/* website title that links back to the home page */}
+
+            <div id="navbar" className="navbar-collapse">
+                <ul>
+
+                    {/* All the pages, clickable titles that link to them  */}
+                    
+                    <li>
+                    <a href="/items">Items</a>
+                    </li>
+
+                    <li>
+                        <a href="/login">Login</a>
+                    </li>
+
+                    <li>
+                        <a href="/cart">Cart</a>
+                    </li>
+
+                    <li>
+                        <a href="/transactions">Transactions</a>
+                    </li>
+
+
+
+
+                </ul>
+
+            </div>
+
+        </nav>
 
 
 
