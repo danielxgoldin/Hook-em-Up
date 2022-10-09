8b15dc5f9760cd22d24cbc462626211c14bfc241
mitchgm <mitchgm@bgsu.edu>
2022-10-09 11:38:34 -0700

    setup

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -2,3 +2,36 @@
 import React from "react"
 import { Link } from "react-router-dom";
 // importing react.js
+
+
+
+// calls the navbar
+
+var navBar = ({}) => {
+
+    return (
+        
+
+
+
+    );
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default navBar; 
