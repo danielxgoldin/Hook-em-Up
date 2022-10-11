95dc9e88e66cb5a6af90d196c414c1f49956d6cc
mitchgm <mitchgm@bgsu.edu>
2022-10-11 13:14:45 -0700

    updated product cards

--- src/Items/Items.jsx
+++ src/Items/Items.jsx
@@ -1,14 +1,16 @@
-
 import React from "react";
 
+function ItemCard() {
+  // Submit button functionality
+  return (
+    <div className="card">
+      <a>
+        <image src="https://via.placeholder.com/150"></image>
+      </a>
+      <h3>Shoes/ITEM NAME</h3>
+      <button>Button</button>
+    </div>
+  );
+}
 
-
-render()
-
-
-
-
-
-
-
-
+export default ItemCard;
--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,71 +1,42 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
 // importing react.js
 
-
-
 // calls the navbar
 
 var navBar = ({}) => {
+  return (
+    <nav className="navbar navbar-dark">
+      {/* navbar title  */}
 
-    return (
-        <nav className="navbar navbar-dark">
-            {/* navbar title  */}
+      <a href="/" className="navbar-title">
+        Hook Em Up
+      </a>
+      {/* website title that links back to the home page */}
 
-            <a href="/#" className="navbar-title">Hook Em Up</a>
-            {/* website title that links back to the home page */}
+      <div id="navbar" className="navbar-collapse">
+        <ul>
+          {/* All the pages, clickable titles that link to them  */}
 
-            <div id="navbar" className="navbar-collapse">
-                <ul>
+          <li className="navTitle">
+            <a href="/items">Items</a>
+          </li>
 
-                    {/* All the pages, clickable titles that link to them  */}
+          <li className="navTitle">
+            <a href="/login">Login</a>
+          </li>
 
-                    <li className="navTitle">
-                    <a href="/items">Items</a>
-                    </li>
+          <li className="navTitle">
+            <a href="/cart">Cart</a>
+          </li>
 
-                    <li className="navTitle">
-                        <a href="/login">Login</a>
-                    </li>
-
-                    <li className="navTitle">
-                        <a href="/cart">Cart</a>
-                    </li>
-
-                    <li className="navTitle">
-                        <a href="/transactions">Transactions</a>
-                    </li>
-
-
-
-
-                </ul>
-
-            </div>
-
-        </nav>
-
-
-
-    );
-
+          <li className="navTitle">
+            <a href="/transactions">Transactions</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-export default navBar; 
+export default navBar;
