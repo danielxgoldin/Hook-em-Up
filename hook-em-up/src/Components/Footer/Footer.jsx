8d2573284d0b639bb353412da307a2b488a01c32
Zaidi <zaidiahsan.r@gmail.com>
2022-10-12 19:43:02 -0500

    footer

--- hook-em-up/src/Components/Footer/Footer.jsx
+++ hook-em-up/src/Components/Footer/Footer.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './footer.css'
-/*import { GiBullHorns } from 'react-icons/gi'*/
+import { GiBullHorns } from 'react-icons/gi'
+import { AiFillTwitterSquare } from 'react-icons/ai'
+import { BsInstagram } from 'react-icons/bs'
+import { AiFillFacebook } from 'react-icons/ai'
+
 
 const Footer = () => {
   return (
     <footer>
-        <a href='#' className='footer__logo'>Logo</a>
+        <a href='#' className='footer__logo'><GiBullHorns /></a>
 
         <div className='footer__text'>
             <p>Hook-em-up</p>
@@ -13,9 +17,9 @@ const Footer = () => {
         </div>
 
         <div className='footer__socials'>
-            <a href='https://facebook.com'></a>
-            <a href='https://instagram.com'></a>
-            <a href='https://twitter.com'></a>
+            <a href='https://facebook.com'><AiFillFacebook /></a>
+            <a href='https://instagram.com'><BsInstagram /></a>
+            <a href='https://twitter.com'><AiFillTwitterSquare /></a>
         </div>
     </footer>
   )
